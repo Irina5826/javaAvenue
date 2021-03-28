c7328bb56f3bc8f7fa3aa256f2d9a78c8bccf1a1
Irina5826 <81438403+Irina5826@users.noreply.github.com>
2021-03-28 19:59:12 +0300

    Create Main.java

---
+++ task_1.1/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = ("Захарова");
+        int age = (36);
+        String city = ("Москва");
+        System.out.println("Захарова 36 лет город Москва");
+
+    }
+}
