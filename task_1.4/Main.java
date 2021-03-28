f4d83204b08a4b31d9350f1e0a05af2a24145b03
Irina5826 <81438403+Irina5826@users.noreply.github.com>
2021-03-28 21:13:39 +0300

    Update Main.java

--- task_1.4/Main.java
+++ task_1.4/Main.java
@@ -1,13 +1,12 @@
-public class Main {
 
+public class Main {
     public static void main(String[] args) {
         int number = 25;
         int a = 5;
-        int b = 5;
-        System.out.println(a*b);
+        int root = number/a;
+        System.out.println("корень из 25 = " + root );
 
-
-
-
-    }
 }
+
+
+        }
