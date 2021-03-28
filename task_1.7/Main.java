3e840de1e1678b2918efaec12d18badfe6610d19
Irina5826 <81438403+Irina5826@users.noreply.github.com>
2021-03-28 20:35:51 +0300

    Create Main.java

---
+++ task_1.7/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 5;
+        int c = 2;
+        int d = 1000;
+        System.out.println(a * b * c * d);
+
+
+    }
+}
