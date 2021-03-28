3f3879dddc37a92bf568d0b357f609b85acfaef4
Irina5826 <81438403+Irina5826@users.noreply.github.com>
2021-03-28 20:09:35 +0300

    Create Main.java

---
+++ задача_1.2/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+        System.out.println("два");
+        System.out.println("плюс");
+        System.out.println("три");
+        System.out.println("равно");
+        System.out.println("пять");
+        System.out.println("2 плюс 3 равно 5");
+        System.out.println(a+b);
+
+    }
+}
