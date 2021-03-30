cfc83ff1f59bdeb34129e493afbcd01644cd09e4
Irina5826 <81438403+Irina5826@users.noreply.github.com>
2021-03-30 23:08:04 +0300

    Update Main.java

--- task_1.3/Main.java
+++ task_1.3/Main.java
@@ -1,11 +1,14 @@
-public class Main {
 
+public class Main {
     public static void main(String[] args) {
-        int a = 7;
-        int b = 9;
-        String S = ("площадь");
-       
+        int a = 3;
+        int b = 2;
+        String s = "sum";
+        int sum = a+b;
+        System.out.println(sum);
 
 
     }
 }
+
+
