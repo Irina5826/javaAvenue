4c4267ca2b5251a0916506972fa300be59e732ab
Irina5826 <81438403+Irina5826@users.noreply.github.com>
2021-03-30 22:40:06 +0300

    Update Main.java

--- task_1.1/Main.java
+++ task_1.1/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String name = ("Захарова");
-        int age = (36);
-        String city = ("Москва");
-        System.out.println("Захарова 36 лет город Москва");
-
-    }
-}
+        String name = "Ирина";
+        int age = 36;
+        String city = "Москва";
+        System.out.println(name+" "+age+" "+city);
