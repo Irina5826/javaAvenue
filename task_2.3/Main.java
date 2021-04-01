87e81c3a5df897ce1d67d999885ac3a10f0b9ea5
Irina5826 <81438403+Irina5826@users.noreply.github.com>
2021-04-01 19:07:07 +0300

    Create Main.java

---
+++ task_2.3/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "8";
+        for (int count = 0; count < 10; count++){
+            System.out.println(s);
+            s = s + 8;
+            }
+        }
+    }
