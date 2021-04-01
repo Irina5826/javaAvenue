038ffc09f9e89cdd6eb33b31c3df481ccda57f53
Irina5826 <81438403+Irina5826@users.noreply.github.com>
2021-04-01 18:59:19 +0300

    Create Main.java

---
+++ задача_2.2/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7,8,9,10};
+        for (int i = 1; i < 11; i++) {
+            for (int k = 1; k < 11; k++) {
+                System.out.print(k * i + "  ");
+            }
+            System.out.println("");
+        }
+    }
+}
