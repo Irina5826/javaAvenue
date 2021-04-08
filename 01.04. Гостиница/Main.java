dba0a5702d3b69e4cb5cf9dc7c777a84009bad01
Irina5826 <81438403+Irina5826@users.noreply.github.com>
2021-04-08 16:29:11 +0300

    Update Main.java

--- 01.04. Гостиница/Main.java
+++ 01.04. Гостиница/Main.java
@@ -1,30 +1,38 @@
+package Hotel;
+
+import Hotel.Hotel;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         Hotel marriott = new Hotel(new Room[]
                 {
-                        new Room(2,true,true,false,false,11),
-                        new Room(2,true,true,false,false,12),
-                        new Room(3,true,true,false,false,13),
-                        new Room(3,true,true,true,true,21),
-                        new Room(4,true,true,false,false,22),
-                        new Room(2,false,false,false,false,23),
-                        new Room(2,false,true,false,false,31),
-                        new Room(1,false,true,true,true,32),
-                        new Room(1,true,true,true,true,33)
+                        new Room(2, true, true, false, false, 11),
+                        new Room(2, true, true, false, false, 12),
+                        new Room(3, true, true, false, false, 13),
+                        new Room(3, true, true, true, true, 21),
+                        new Room(4, true, true, false, false, 22),
+                        new Room(2, false, false, false, false, 23),
+                        new Room(2, false, true, false, false, 31),
+                        new Room(1, false, true, true, true, 32),
+                        new Room(1, true, true, true, true, 33)
                 });
-                while (true){
-                    System.out.println("Введите команду");
-                    String command = scanner.nextLine();
-                    if (command.equals("eat"+"shower"+ "toilet"+"wifi")) {
-                        if(command.equals("freeRooms")){
-                            marriott.getFreeRooms();
-                        }
-                    } else {
-                        marriott.getRoomsWithEat();
-                    }
-                }
+        while (true) {
+            System.out.println("Введите команду");
+            String command = scanner.nextLine();
+            if (command.equals("eat" )) {
+                marriott.getRoomsWithEat();
+            } else if (command.equals("freeRooms")) {
+                marriott.getFreeRooms();
+            } else if (command.indexOf("place") == 0) {
+                int place = Integer.parseInt(String.valueOf(command.charAt(6)));
+                marriott.getRoomsWithPlace(place);
+            }
+
+        }
+
     }
 }
