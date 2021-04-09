ef95e709e9b1283646b8a5edab04ce12cb66c71c
Irina5826 <81438403+Irina5826@users.noreply.github.com>
2021-04-09 20:40:56 +0300

    Create Main.java

---
+++ task_1.5/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Task {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        System.out.println((a * 9 / 5) + 32);
+
+    }
+}
