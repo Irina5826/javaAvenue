f544947c57c163d79df0680ffcba59a5f08ee267
Irina5826 <81438403+Irina5826@users.noreply.github.com>
2021-04-20 01:05:41 +0300

    Update Main.java

--- 08.04/Main.java
+++ 08.04/Main.java
@@ -1,53 +1,90 @@
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URL;
 
-public class Task {
+public class Main {
     public static void main(String[] args) {
+
         try {
             FileReader reader = new FileReader("C:\\Users\\asus\\Documents\\test.txt");
+            FileWriter writer = new FileWriter("C:\\Users\\asus\\Documents\\good.txt");
             int c;
-            String ip = "";
-            String port = "";
+            String ip ="";
+            String port ="";
             String proxy = "";
-            while ((c = reader.read()) != -1){
-                if (c==13) continue;
-                if(c == 10){
+            while ((c = reader.read()) != -1) {
+                if (c == 13) continue;
+                else if (c == 10) {
                     ip = proxy.split(":")[0];
-                   port=proxy.split(":")[1];
-                   proxy = "";
-                    System.out.println("ip: "+ip+"port: "+port);
-                }
-                else if (c != 9) {
-                    proxy += (char)c;
-                }else {
+                    port= proxy.split(":")[1];
+                    proxy = "";
+                    String finalIp = ip;
+                    String finalPort = port;
+                    Thread thread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            String result = checkProxy(finalIp, Integer.parseInt(finalPort));
+                            if (result.equals("error"))
+                                System.out.println(finalIp + " не работает");
+                            else {
+                                try {
+                                    writer.write(finalIp+":"+finalPort+"\n");
+                                    writer.flush();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+
+                                System.out.println(finalIp + " работает");
+                            }
+                        }
+                    });
+                    thread.start();
+
+                } else if (c != 9) {
+                    proxy += (char) c;
+                } else {
                     proxy += ":";
                 }
-
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
-
-
-
-
-
-
-
-
         /*try {
-            FileWriter writer = new FileWriter("C:\\Users\\asus\\Documents\\test.txt", true);
-            String text = "Hello Word";
+            FileWriter writer = new FileWriter("C:\\java\\test.txt",true);
+            String text = "Hello world";
             writer.write(text);
             writer.append("\n");
-            writer.write("HELLO WORD");
+            writer.write("Hello WORLD");
             writer.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }*/
+    }
+
+    static String checkProxy(String ip, int port) {
+        HttpURLConnection connection = null;
+        try {
+            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(ip, port));
+            URL url = new URL("https://vozhzhaev.ru/test.php");
+            connection = (HttpURLConnection) url.openConnection(proxy);
+            InputStream is = connection.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
+            String line;
+            while ((line = rd.readLine()) != null) {
+                response.append(line);
+                response.append('\r');
+            }
+            rd.close();
+            return response.toString();
+        } catch (Exception e) {
+            return "error";
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
     }
 }
