496f7a4aa59313a0e253255a56ed9c5b754d2841
Irina5826 <81438403+Irina5826@users.noreply.github.com>
2021-05-11 12:47:00 +0300

    Create Main.java

---
+++ задача_3.3/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        equ(5, 4);
+
+    }
+
+
+    public static void equ(int a, int b) {
+        if (a == b) System.out.println("true");
+        else System.out.println(false);
+    }
+}
