15021d3631a981ff684e566db2a14a045944749b
Irina5826 <81438403+Irina5826@users.noreply.github.com>
2021-05-11 12:43:41 +0300

    Create Main.java

---
+++ task_2.9/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] num = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] == 5) System.out.println("да");
+            else System.out.println("нет");
+        }
+
+    }
+
+}
