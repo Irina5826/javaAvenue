592c1d0863674eb3d9c07008b0198f4cc68d9ae2
Irina5826 <81438403+Irina5826@users.noreply.github.com>
2021-05-11 12:42:53 +0300

    Create Main.java

---
+++ task_2.8/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] elem = {2, 3, 4, 5};
+        int num = 1;
+        for (int i = 0; i < elem.length; i++) {
+            num *= elem[i];
+            System.out.println(num);
+        }
+
+    }
+}
+ 
