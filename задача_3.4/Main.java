cf13f0bac15348248a60caaba09fce28cf52b1cc
Irina5826 <81438403+Irina5826@users.noreply.github.com>
2021-05-11 12:47:43 +0300

    Create Main.java

---
+++ задача_3.4/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        sum(5, 4);
+
+    }
+
+    static void sum(int a, int b) {
+        if ((a + b) < 10) System.out.println("true");
+        else System.out.println("false");
+    }
+}
